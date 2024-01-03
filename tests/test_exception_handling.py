262940123dd5bc24679aa9865de323b78ec633d0
Cheryl Danner <sheromon@users.noreply.github.com>
2024-01-02 22:50:33 -0500

    Fixed get_exception_handling to be aligned with version on main

--- tests/test_exception_handling.py
+++ tests/test_exception_handling.py
@@ -9,17 +9,12 @@ from outfit_picker.classes import OutfitPicker
 
 
 @patch("sys.stdout", new_callable=io.StringIO)
-def assert_stdout(outfitter, expected_output, mock_stdout):
-    """Verify that the expected message gets printed when recommend_outfit is run."""
-    outfitter.recommend_outfit()
-    assert mock_stdout.getvalue().strip() == expected_output
-
-
-def test_exception_handling():
+def test_exception_handling(mock_stdout):
     """"Verify that if there is a connection error while calling the weather api,
     the expected message gets printed.
     """
     outfitter = OutfitPicker()
     outfitter.openmeteo.weather_api = Mock(side_effect=requests.exceptions.ConnectionError())
+    outfitter.recommend_outfit()
     expected_output = "Weather data is not available. Cannot recommend an outfit."
-    assert_stdout(outfitter, expected_output)
+    assert mock_stdout.getvalue().strip() == expected_output
